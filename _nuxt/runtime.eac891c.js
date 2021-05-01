ba8d6f123967727ef6b89ba0d0c033fa482361ce
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-05-02 00:56:11 +0530

    Update runtime.eac891c.js

--- _nuxt/runtime.eac891c.js
+++ _nuxt/runtime.eac891c.js
@@ -54,9 +54,9 @@
                         9: "pages/cow/hybird",
                         10: "pages/guard",
                         11: "pages/index",
-                        12: "pages/ethb",
-                        13: "pages/ethbbnb",
-                        14: "pages/ethbbusd",
+                        12: "pages/ethbdoge",
+                        13: "pages/ethbada",
+                        14: "pages/ethbdot",
                         15: "pages/pumpkin"
                     }[e] || e) + "." + {
                         0: "0a04846",
